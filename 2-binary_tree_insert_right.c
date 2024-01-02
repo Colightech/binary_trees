d056f02c78426715ff5544afec707541598f0ae1
Colightech <saintchris4@gmail.com>
2024-01-02 23:31:53 +0100

    3-binary_tree_delete.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * 
+ * binary_tree_insert_right - function that inserts
+ * a node as the right-child of another node
+ * @parnet: pointer to the node to insert the right-child
+ * @value: value to store in the new node
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
