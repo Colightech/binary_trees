d927f49b1e3ee926374f58e6263523682f2a9ecb
Colightech <saintchris4@gmail.com>
2024-01-02 22:47:51 +0100

    updating header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,11 @@
-#ifndef _BINARY_TREE_H
-#define _BINARY_TREE_H
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -22,6 +24,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
