996593207407893e3c30c80b7cfceea8999e2e32
Colightech <saintchris4@mail.com>
2024-01-03 02:31:44 -0800

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -52,14 +52,14 @@ const binary_tree_t *get_leaf(const binary_tree_t *tree)
  * Return: If the tree is perfect, 1, otherwise 0.
  */
 int is_perfect_recursive(const binary_tree_t *tree,
-		size_t leaf_dept, size_t leaf_level)
+		size_t leaf_depth, size_t level)
 {
 	if (is_leaf(tree))
-		return (leaf_level == leaf_dept ? 1 : 0);
+		return (level == leaf_depth ? 1 : 0);
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
-	return (is_perfect_recursive(tree->left, leaf_dept, leaf_level + 1) &&
-		is_perfect_recursive(tree->right, leaf_dept, leaf_level + 1));
+	return (is_perfect_recursive(tree->left, leaf_depth, level + 1) &&
+		is_perfect_recursive(tree->right, leaf_depth, level + 1));
 }
 
 /**
