39314539a7e28b6dea11ae0b3b7f3bf597f0eef7
Colightech <saintchris4@mail.com>
2024-01-03 01:35:23 -0800

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,8 +3,9 @@
 /**
  * binary_tree_insert_right - function that inserts
  * a node as the right-child of another node
- * @parnet: pointer to the node to insert the right-child
+ * @parent: pointer to the node to insert the right-child
  * @value: value to store in the new node
+ * Return: return a pointer to the created node
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
@@ -23,6 +24,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right->parent = newNode;
 	}
 	parent->right = newNode;
-
 	return (newNode);
 }
