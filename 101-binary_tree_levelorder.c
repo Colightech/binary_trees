6dd38fc25a36f9f11ff7797624af08af03e6664a
Colightech <saintchris4@mail.com>
2024-01-05 03:30:28 -0800

    updating binary tree task

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -7,14 +7,15 @@
  */
 typedef struct node_s
 {
-	const binary_tree_t *node;
-	struct node_s *next;
+        const binary_tree_t *node;
+        struct node_s *next;
 } ll;
 
 ll *append(ll *head, const binary_tree_t *btnode);
 void free_list(ll *head);
 ll *get_children(ll *head, const binary_tree_t *parent);
 void levels_rec(ll *head, void (*func)(int));
+
 
 /**
  * binary_tree_levelorder - Goes through a binary tree
@@ -78,7 +79,7 @@ ll *append(ll *head, const binary_tree_t *btnode)
 {
 	ll *newNode = NULL, *last = NULL;
 
-	newNode = malloc(sizeof(*new));
+	newNode = malloc(sizeof(*newNode));
 	if (newNode)
 	{
 		newNode->node = btnode;
